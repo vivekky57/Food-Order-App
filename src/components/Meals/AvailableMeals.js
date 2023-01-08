8117f96fa56288f51f0beeb8d8868f28b0a37d8b
vivekky <vivekky57@gmail.com>
2023-01-08 06:01:42 +0530

    Managing Cart & Modal State

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,6 +31,7 @@ const DUMMY_MEALS = [
 const availableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
