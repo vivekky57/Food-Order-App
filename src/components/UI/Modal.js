3b47c881551c13f639e316d8106f74129c82a4a4
vivekky <vivekky57@gmail.com>
2023-01-09 04:04:04 +0530

    Adding cart Context

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,8 +2,9 @@ import { Fragment } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 const Backdrop = (props) => {
-  return <div className={classes.Backdrop} onClick={props.onClose} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
+
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
